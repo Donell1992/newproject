4574595b9a33f5fbe09f633a9e205dbff3830ead
Donell1992 <dmw9292@yahoo.com>
2021-10-21 15:19:47 -0500

    Addition of setters

--- MixedMessages/MixedMessages.js
+++ MixedMessages/MixedMessages.js
@@ -6,14 +6,37 @@ so far to build the program and customize it to your liking.*/
 
  const message() {
      return {
-         _names: names,
-         _message1: message1,
-         _message2: message2,
-         _message3: message3,
+         _names: [],
+         _messages1: [],
+         _messagea2: [],
+         _messagea3: [],
 
-         const randomMessage1 () {
-             
+         get names() {
+             return this._names 
+         },
+         get message1(){
+             return this._message1
+         },
+         get message2(){
+             return this._message2
+         },
+         get message3(){
+             return this._message3
+         },
+         set names(named){
+             // add name to array
+             this._names.push(named) 
+         },
+         set message1(messaged1){
+             this._message1.push(messaged1)
+         },
+         set message2(messaged2){
+             this._message2.push(messaged2)
+         },
+         set message3(messaged3){
+             this._message3.push(messaged3)
          }
+
 
 
      }
